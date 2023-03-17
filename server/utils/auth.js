a42bad5ac598b14b61bbeb16fdef3446087796e0
kait <kingkittykait@gmail.com>
2023-03-17 15:39:09 -0700

    made dotenv file, updated authentication file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,32 +1,17 @@
-const jwt = require('jsonwebtoken');
-const secret = 'supersecret';
-const expiration = '2h';
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+
+
+const secret = process.env.JWT_SECRET
+const expiration = process.env.JWT_EXPIRATION
+
+
+function signToken({ _id, username, email }) {
+	const payload = { _id, username, email }
+	return jwt.sign({ data: payload }, secret, { expiresIn: expiration },
+	)
+}
 
 module.exports = {
-  authMiddleware: function (req, res, next) {
-    let token = req.query.token || req.headers.authorization;
-
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
-
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
-
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
-
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
-};
+	signToken,
+}
