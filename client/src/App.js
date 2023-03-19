d1ea64074ed687e084cd86db9f9b9a4a47ca0d2c
kait <kingkittykait@gmail.com>
2023-03-19 16:00:44 -0700

    build failed due to router term not exported

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks.js';
 import SavedBooks from './pages/SavedBooks.js';
 import Navbar from './components/Navbar.js';
@@ -16,11 +16,11 @@ function App() {
       <Router>
         <>
       <Navbar />
-          <Switch>
+          <Routes>
             <Route exact path="/" component={SearchBooks} />
             <Route exact path="/saved" component={SavedBooks} />
             <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-          </Switch>
+          </Routes>
           </>
     </Router>
     </ApolloProvider>
